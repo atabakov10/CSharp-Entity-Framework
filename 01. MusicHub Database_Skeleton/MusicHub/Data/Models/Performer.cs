649576192d74f19f7c17e89ccb90540f172087ba
Angel Tabakov <atabakov99@abv.bg>
2022-08-13 19:26:34 +0300

    100/100

---
+++ 01. MusicHub Database_Skeleton/MusicHub/Data/Models/Performer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using MusicHub.Common;
+
+namespace MusicHub.Data.Models
+{
+    public class Performer
+    {
+        public Performer()
+        {
+            PerformerSongs = new HashSet<SongPerformer>();
+        }
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(GlobalConstants.PerformerNameMaxLength)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(GlobalConstants.PerformerNameMaxLength)]
+        public string LastName { get; set; }
+
+        [Required]
+        public int Age { get; set; }
+
+        [Required ]
+        public decimal NetWorth { get; set; }
+
+        public virtual ICollection<SongPerformer> PerformerSongs { get; set; }
+
+    }
+}
